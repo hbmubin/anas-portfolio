06b8b59f054d46aa66a611ba1b1358ef40b92bbb
hbmubin <hasanulbannamubin01@gmail.com>
2024-11-03 10:29:24 +0600

    footer text fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,11 @@
 const Footer = () => {
   return (
     <footer className="bg-bodyBlack py-10 text-center">
-      <span className="text-white">
-        Copyright &copy; 2024 by{" "}
+      <span className="text-white block mb-2">
+        Copyright &copy; 2024 by Anas Munir
+      </span>
+      <span className="text-neutral-400 block text-sm">
+        Developed by{" "}
         <a
           className="hover:text-blue-700 duration-200 underline text-sm"
           target="_blank"
